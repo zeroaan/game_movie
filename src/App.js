62364bc221aa9a63359acb162375c736656b5cd3
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-09-22 08:28:26 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Search from "./routes/Search";
+import Movies from "./routes/Movies";
 
 function App() {
   return (
     <HashRouter>
       <Route path="/" exact={true} component={Search} />
+      <Route path="/movies" component={Movies} />
     </HashRouter>
   );
 }
